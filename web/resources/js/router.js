c671d33af3af7c3fa44021130198812df03a4b00
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-11 08:21:55 +0900

    add:routerの設定

---
+++ web/resources/js/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネントをインポートする
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを使用する
+// これによって<RouterView />コンポーネントなどを使うことができる
+Vue.use(VueRouter)
+
+// パスとコンポーネントのマッピング
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+// VueRouterインスタンスを作成する
+const router = new VueRouter({
+  routes
+})
+
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
+export default router
