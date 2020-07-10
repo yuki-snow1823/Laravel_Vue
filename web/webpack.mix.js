e77791ff554133b1939ff7f599380c1132f0925a
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-11 07:32:33 +0900

    add:mixjsの変更

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -1,15 +1,11 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+  .browserSync("vuesplash.test")
+  .js("resources/js/app.js", "public/js")
+  .version();
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.browserSync({
+  proxy: "0.0.0.0:8081", // アプリの起動アドレス
+  open: false // ブラウザを自動で開かない
+});
