b9bf5d25a1f7f534408ee5883934c705d6ef1ca1
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-11 16:58:53 +0900

    add:ログインメソッド追加

--- web/routes/api.php
+++ web/routes/api.php
@@ -18,4 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
     // 会員登録
     Route::post('/register', 'Auth\RegisterController@register')->name('register');
+    // ログイン
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+
 });
--- web/routes/web.php
+++ web/routes/web.php
@@ -21,3 +21,4 @@ Route::get('/{any?}', fn () => view('index'))->where('any', '.+');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
