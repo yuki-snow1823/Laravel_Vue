4a2f4e7e7fa833ff856619a43686a0dc4b03e2d8
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-11 16:07:57 +0900

    add:auth機能の追加

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -1,11 +1,15 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
-mix
-  .browserSync("vuesplash.test")
-  .js("resources/js/app.js", "public/js")
-  .version();
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-mix.browserSync({
-  proxy: "0.0.0.0:8081", // アプリの起動アドレス
-  open: false // ブラウザを自動で開かない
-});
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
