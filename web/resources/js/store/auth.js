63bcbd835114fc0c11bea3da5a5f089f96949177
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-14 22:48:23 +0900

    add:login時にapiチェック

--- web/resources/js/store/auth.js
+++ web/resources/js/store/auth.js
@@ -1,11 +1,12 @@
-import { OK } from "../util";
+import { OK, CREATED, UNPROCESSABLE_ENTITY } from "../util";
 
 const state = {
   user: null,
-  apiStatus: null
+  apiStatus: null,
+  loginErrorMessages: null,
+  registerErrorMessages: null
 };
 
-// getterの追加
 const getters = {
   check: state => !!state.user,
   username: state => (state.user ? state.user.name : "")
@@ -15,21 +16,41 @@ const mutations = {
   setUser(state, user) {
     state.user = user;
   },
-  setApiStatus (state, status) {
-    state.apiStatus = status
+  setApiStatus(state, status) {
+    state.apiStatus = status;
+  },
+  setLoginErrorMessages(state, messages) {
+    state.loginErrorMessages = messages;
+  },
+  setRegisterErrorMessages(state, messages) {
+    state.registerErrorMessages = messages;
   }
 };
 
 const actions = {
+  // 会員登録
   async register(context, data) {
+    context.commit("setApiStatus", null);
     const response = await axios.post("/api/register", data);
-    context.commit("setUser", response.data);
+
+    if (response.status === CREATED) {
+      context.commit("setApiStatus", true);
+      context.commit("setUser", response.data);
+      return false;
+    }
+
+    context.commit("setApiStatus", false);
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      context.commit("setRegisterErrorMessages", response.data.errors);
+    } else {
+      context.commit("error/setCode", response.status, { root: true });
+    }
   },
+
+  // ログイン
   async login(context, data) {
     context.commit("setApiStatus", null);
-    const response = await axios
-      .post("/api/login", data)
-      .catch(err => err.response || err); // ?
+    const response = await axios.post("/api/login", data);
 
     if (response.status === OK) {
       context.commit("setApiStatus", true);
@@ -38,16 +59,42 @@ const actions = {
     }
 
     context.commit("setApiStatus", false);
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      context.commit("setLoginErrorMessages", response.data.errors);
+    } else {
+      context.commit("error/setCode", response.status, { root: true });
+    }
+  },
+
+  // ログアウト
+  async logout(context) {
+    context.commit("setApiStatus", null);
+    const response = await axios.post("/api/logout");
+
+    if (response.status === OK) {
+      context.commit("setApiStatus", true);
+      context.commit("setUser", null);
+      return false;
+    }
+
+    context.commit("setApiStatus", false);
     context.commit("error/setCode", response.status, { root: true });
   },
-  async logout(context) {
-    const response = await axios.post("/api/logout");
-    context.commit("setUser", null);
-  },
+
+  // ログインユーザーチェック
   async currentUser(context) {
+    context.commit("setApiStatus", null);
     const response = await axios.get("/api/user");
     const user = response.data || null;
-    context.commit("setUser", user);
+
+    if (response.status === OK) {
+      context.commit("setApiStatus", true);
+      context.commit("setUser", user);
+      return false;
+    }
+
+    context.commit("setApiStatus", false);
+    context.commit("error/setCode", response.status, { root: true });
   }
 };
 
