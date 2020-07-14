aa2cca8a87c0eb31472c07f92f8fd93e35cca069
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-14 23:18:17 +0900

    add:statusメッセージの定義

--- web/resources/js/util.js
+++ web/resources/js/util.js
@@ -5,7 +5,6 @@
  */
 export function getCookieValue(searchKey) {
   if (typeof searchKey === "undefined") {
-    console.log("undefine")
     return "";
   }
 
@@ -20,3 +19,8 @@ export function getCookieValue(searchKey) {
 
   return val;
 }
+
+export const OK = 200;
+export const CREATED = 201;
+export const INTERNAL_SERVER_ERROR = 500;
+export const UNPROCESSABLE_ENTITY = 422;
