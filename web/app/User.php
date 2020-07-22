c96bf89bcb2f96b451332c56782f0908b62438e2
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-22 22:13:14 +0900

    add:リレーション追記

--- web/app/User.php
+++ web/app/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    protected $visible = [
+    'name',
+];
+
     /**
      * The attributes that should be hidden for arrays.
      *
