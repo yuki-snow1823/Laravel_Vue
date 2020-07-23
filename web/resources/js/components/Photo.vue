dacbe2b0c217b32873c2238c7a96a824cda56176
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-23 18:47:54 +0900

    add:コンポーネントの変更

--- web/resources/js/components/Photo.vue
+++ web/resources/js/components/Photo.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="photo">
     <figure class="photo__wrapper">
-      <img
-        class="photo__image"
-        :src="item.url"
-        :alt="`Photo by ${item.owner.name}`"
-      >
+      <img class="photo__image" :src="item.url" :alt="`Photo by ${item.owner.name}`">
     </figure>
-    <RouterLink
-      class="photo__overlay"
-      :to="`/photos/${item.id}`"
-      :title="`View the photo by ${item.owner.name}`"
-    >
+    <RouterLink class="photo__overlay" :to="`/photos/${item.id}`" :title="`View the photo by ${item.owner.name}`">
       <div class="photo__controls">
-        <button
-          class="photo__action photo__action--like"
-          title="Like photo"
-        >
-          <i class="icon ion-md-heart"></i>12
+        <button class="photo__action photo__action--like" :class="{ 'photo__action--liked': item.liked_by_user }"
+          title="Like photo" @click.prevent="like">
+          <i class="icon ion-md-heart"></i>{{ item.likes_count }}
         </button>
-        <a
-          class="photo__action"
-          title="Download photo"
-          @click.stop
-          :href="`/photos/${item.id}/download`"
-        >
+        <a class="photo__action" title="Download photo" @click.stop :href="`/photos/${item.id}/download`">
           <i class="icon ion-md-arrow-round-down"></i>
         </a>
       </div>
@@ -36,12 +21,21 @@
 </template>
 
 <script>
-export default {
-  props: {
-    item: {
-      type: Object,
-      required: true
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      like() {
+        this.$emit('like', {
+          id: this.item.id,
+          liked: this.item.liked_by_user
+        })
+      }
     }
   }
-}
+
 </script>
